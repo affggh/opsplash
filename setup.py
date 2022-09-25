94765038f9af4723b63ff1fdc0485edf2dfd7a62
affggh <46290091+affggh@users.noreply.github.com>
2022-09-25 21:00:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import Extension, setup
 
 from distutils.core import setup, Extension #这里要用到distutils库
-module = Extension('opsplash', sources = ['pyopsplash.c'],
+module = Extension('opsplash', sources = ['src/pyopsplash.c'],
                      extra_compile_args=['-Wall',
                                          '-O2'],
-                     include_dirs=["."],
+                     include_dirs=["include"],
                      libraries=['z']  # zlib
                      )
 setup (name = 'opsplash', #打包名
